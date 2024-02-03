56d358dc7392057b57e689e437e343cd8f15c53b
Amer Said <156580420+amersaid811@users.noreply.github.com>
2024-02-03 19:22:51 +0200

    in the traingleclassifier class, line 26 we changed the triangle type to equilateral because it was missed and there was duplicate SCALENE traingle

--- Traingle_Casification_class_1/src/main/java/Traingle_Casification_class_1/Traingle_Casification_class_1/App.java
+++ Traingle_Casification_class_1/src/main/java/Traingle_Casification_class_1/Traingle_Casification_class_1/App.java
@@ -23,7 +23,7 @@ class TriangleClassifier {
 			
 	        
 	        if (isEquilateral(a, b, c)) {
-	        	triangle= Triangle_Types.equilateral;
+	        	triangle= Triangle_Types.equilateral; //here was the error//
 	        }else if(isIsosceles(a, b, c)) {
 	        	triangle= Triangle_Types.isosceles;
 	        }else if(isTriangle(a, b, c)){
