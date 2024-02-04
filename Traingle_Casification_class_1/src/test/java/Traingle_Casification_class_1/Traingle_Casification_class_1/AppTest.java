8e270cd0ac320ae18076e3303d4a37cf9bc55711
Amer Said <156580420+amersaid811@users.noreply.github.com>
2024-02-04 13:21:03 +0200

    testing

--- Traingle_Casification_class_1/src/test/java/Traingle_Casification_class_1/Traingle_Casification_class_1/AppTest.java
+++ Traingle_Casification_class_1/src/test/java/Traingle_Casification_class_1/Traingle_Casification_class_1/AppTest.java
@@ -11,7 +11,7 @@ import  org.junit.Test;
 public class AppTest {
 	
 	TriangleClassifier triangle = new TriangleClassifier();
-	
+/*******************************************EP TEST****************************************/
 	@Test
 	public void equilateralTriangle(){
         int a = 2;
@@ -56,7 +56,7 @@ public class AppTest {
         String result = triangle.get_type().toString();
         assertEquals(expected, result.toString());
     }
-
+	//test
 	@Test
 	public void ErrorTriangle(){
         int a = -7;
@@ -67,5 +67,68 @@ public class AppTest {
         String result = triangle.get_type().toString();
         assertEquals(expected, result.toString());
     }
+	
+	
+	
+	/*******************************************BVA TEST****************************************/
+	@Test
+	/*** A (MIN) ***/
+	public void BVANotrTriangle(){
+        int a = 0;
+        int b = 60;
+        int c = 30;
+        triangle.classifyTriangle(a,b,c);
+        String expected =Triangle_Types.Nottriangle.toString();
+        String result = triangle.get_type().toString();
+        assertEquals(expected, result.toString());
+    }
+	@Test
+	/*** A (MIN+1) ***/
+	public void BVAisoscelesTriangle(){
+        int a = 1;
+        int b = 50;
+        int c = 50;
+        triangle.classifyTriangle(a,b,c);
+        String expected =Triangle_Types.isosceles.toString();
+        String result = triangle.get_type().toString();
+        assertEquals(expected, result.toString());
+    }
+	@Test
+	/*** A (Nominal) ***/
+	public void BVAequilateralTriangle(){
+        int a = 50;
+        int b = 50;
+        int c = 50;
+        triangle.classifyTriangle(a,b,c);
+        String expected =Triangle_Types.equilateral.toString();
+        String result = triangle.get_type().toString();
+        assertEquals(expected, result.toString());
+    }
+	@Test
+	/*** A (max-1) ***/
+	public void BVAScaleneTriangle(){
+        int a = 99;
+        int b = 76;
+        int c = 47;
+        triangle.classifyTriangle(a,b,c);
+        String expected =Triangle_Types.Scalene.toString();
+        String result = triangle.get_type().toString();
+        assertEquals(expected, result.toString());
+    }
+	@Test
+	/*** A (max) ***/
+	public void BVAErrorTriangle(){
+        int a = 100;
+        int b = -1;
+        int c = -5;
+        triangle.classifyTriangle(a,b,c);
+        String expected =Triangle_Types.ERROR.toString();
+        String result = triangle.get_type().toString();
+        assertEquals(expected, result.toString());
+    }
+	
+	
+
 }  
+
 
