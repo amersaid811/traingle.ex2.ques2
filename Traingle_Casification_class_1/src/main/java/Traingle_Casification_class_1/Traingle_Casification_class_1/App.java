a344d422048925e37dca40eefeeeade961b9f20a
Amer Said <156580420+amersaid811@users.noreply.github.com>
2024-02-04 15:19:22 +0200

    we have dealt with the errors in roots.java

--- Traingle_Casification_class_1/src/main/java/Traingle_Casification_class_1/Traingle_Casification_class_1/App.java
+++ Traingle_Casification_class_1/src/main/java/Traingle_Casification_class_1/Traingle_Casification_class_1/App.java
@@ -55,7 +55,7 @@ class TriangleClassifier {
 	        return (a + b > c && a + c > b && b + c > a) && (a!=b && a!=c && b!=c);  
 	    }
 	    private static boolean isError(int a, int b, int c) {
-	        return a<=0 || b<=0 || c<=0;
+	        return a<0 || b<0 || c<0;
 	    }
 	    
 	    public static Triangle_Types get_type() {
